0e4555941b6292f711a9e0160f631f3002e0add0
Harsimran Singh Maan <maan.harry@gmail.com>
2024-06-24 12:21:01 -0700

    update deps

--- main.tf
+++ main.tf
@@ -1,13 +1,12 @@
 terraform {
-  required_version = "1.8.3"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.103.1"
+      version = ">= 3.109.0"
     }
     azapi = {
       source  = "azure/azapi"
-      version = "1.13.1"
+      version = ">= 1.13.1"
     }
   }
 }
